0a56db4d1fd1dd913043fd06116fb4e608a96c18
rencc <rccman@163.com>
2017-09-19 12:02:39 +0800

    Initial commit

---
+++ src/main/java/com/rencc/wexin/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.rencc.wexin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试Controller
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
